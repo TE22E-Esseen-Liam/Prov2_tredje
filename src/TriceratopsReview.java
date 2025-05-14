25f98bcf10aeab8c488904039b6e149300cfb35f
TE22E-Esseen-Liam <143697999+TE22E-Esseen-Liam@users.noreply.github.com>
2025-05-14 22:35:59 +0200

    Kommentarer

--- src/TriceratopsReview.java
+++ src/TriceratopsReview.java
@@ -1,10 +1,11 @@
 import java.util.Random;
 
+// Undersklass till DinoReview, specifik för Triceratops
 public class TriceratopsReview extends DinoReview{
     public TriceratopsReview() {
-        super();
+        super(); // Anropar konstruktorn i basklassen
         this.dinoName = "Triceratops";
         this.dinoDescription = "En växtätare med tre horn och krage.";
-        this.dinoWeight = new Random().nextInt(1000) + 5000;
+        this.dinoWeight = new Random().nextInt(1000) + 5000; // Vikt mellan 5000–5999 kg
     }
 }
