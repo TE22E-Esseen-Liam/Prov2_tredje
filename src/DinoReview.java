df646ea4a7f61670a6ca2973e98251999bfa0553
TE22E-Esseen-Liam <143697999+TE22E-Esseen-Liam@users.noreply.github.com>
2025-05-14 22:34:33 +0200

    Kommentarer

--- src/DinoReview.java
+++ src/DinoReview.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class DinoReview {
-    protected String dinoName;
-    protected String dinoDescription;
-    protected int dinoWeight;
-    protected int grade;
-    protected String reviewText;
+    protected String dinoName; // Namn på dinosaurien
+    protected String dinoDescription; // Kort beskrivning
+    protected int dinoWeight; // Vikt i kg
+    protected int grade; // Betyg (1-5)
+    protected String reviewText; // Recensionstext
 
     Scanner scanner = new Scanner(System.in);
     Random rand = new Random();
@@ -15,6 +15,7 @@ public class DinoReview {
         this.grade = 0;
         this.reviewText = "";
     }
+    // Användaren sätter ett betyg (1-5)
     public void setGrade() {
         while(true) {
             System.out.println("Sätt in betyg för " + dinoName + " (1-5):");
@@ -28,12 +29,13 @@ public class DinoReview {
             }
         }
     }
-
+    // Användaren skriver en kort recension
     public void writeText(){
         System.out.println("Skriv en recension för " + dinoName + ":");
-        scanner.nextLine();
+        scanner.nextLine(); // För att ta bort radbrytning
         this.reviewText = scanner.nextLine();
     }
+    // Returnerar all information som en text
     public String getReview(){
         return "Dinosaurie " + dinoName + "\nBeskrivning: " + dinoDescription + "\nVikt: " + dinoWeight + "kg\nBetyg: " + grade + "\nRecension: " + reviewText + "\n";
     }
