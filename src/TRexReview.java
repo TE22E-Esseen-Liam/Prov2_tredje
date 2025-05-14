ee0d4bd7efa2c46e8972bd173d551e269b8d2e19
TE22E-Esseen-Liam <143697999+TE22E-Esseen-Liam@users.noreply.github.com>
2025-05-14 22:35:22 +0200

    Kommentarer

--- src/TRexReview.java
+++ src/TRexReview.java
@@ -1,11 +1,12 @@
 import java.util.Random;
 
+// Undersklass till DinoReview, specifik för T-Rex
 public class TRexReview extends DinoReview {
     public TRexReview(){
-        super();
+        super(); // Anropar konstruktorn i basklassen
         this.dinoName = "Tyrannosaurus Rex";
         this.dinoDescription = "En stor köttätare";
-        this.dinoWeight = new Random().nextInt(2000) + 6000;
+        this.dinoWeight = new Random().nextInt(2000) + 6000; // Vikt mellan 6000–7999 kg
     }
 }
 
